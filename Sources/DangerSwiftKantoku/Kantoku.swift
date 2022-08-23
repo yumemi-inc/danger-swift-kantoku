dec6e57b320d904226387833c8e5230b37334684
Sudeep Kini <sudeep.kini@lenskart.in>
2022-08-23 19:09:46 +0530

    Add code to PostCodeCoverage
    1. adds 'failIfCoverageUnavailable' to allow user to fail the build if coverage is'nt avaiable
    2. adds 'excludeCoverageTarget' to allow the user to filter out specific targets like pods or tests
    3. adds 'showCoverageForChangedFiles' to allow user to show coverage for code changes and 'excludeCoverageForFiles' to exclude specific files
    4. add code to support the above config vairables
    
    we first filter out the exculded targets, then exludedFiles. After which we proceed to post the
    Overall coverage following the coverage for diff files grouped by target.
    
    https://github.com/yumemi-inc/danger-swift-kantoku/issues/4

--- Sources/DangerSwiftKantoku/Kantoku.swift
+++ Sources/DangerSwiftKantoku/Kantoku.swift
@@ -1,6 +1,6 @@
 //
 //  Kantoku.swift
-//  
+//
 //
 //  Created by 史 翔新 on 2022/02/10.
 //
@@ -9,22 +9,21 @@ import Foundation
 import XCResultKit
 
 public struct Kantoku {
-    
     let workingDirectoryPath: String
     let modifiedFiles: [String]
     let createdFiles: [String]
-    
+
     private let markdownCommentExecutor: (_ comment: String) -> Void
-    
+
     private let inlineCommentExecutor: (_ comment: String, _ filePath: String, _ lineNumber: Int) -> Void
     private let normalCommentExecutor: (_ comment: String) -> Void
-    
+
     private let inlineWarningExecutor: (_ comment: String, _ filePath: String, _ lineNumber: Int) -> Void
     private let normalWarningExecutor: (_ comment: String) -> Void
-    
+
     private let inlineFailureExecutor: (_ comment: String, _ filePath: String, _ lineNumber: Int) -> Void
     private let normalFailureExecutor: (_ comment: String) -> Void
-    
+
     init(
         workingDirectoryPath: String,
         modifiedFiles: [String],
@@ -48,108 +47,104 @@ public struct Kantoku {
         self.inlineFailureExecutor = inlineFailureExecutor
         self.normalFailureExecutor = normalFailureExecutor
     }
-    
 }
 
 extension Kantoku {
-    
     func markdown(_ comment: String) {
         markdownCommentExecutor(comment)
     }
-    
+
     func comment(_ comment: String, to filePath: String, at lineNumber: Int) {
         inlineCommentExecutor(comment, filePath, lineNumber)
     }
-    
+
     func comment(_ comment: String) {
         normalCommentExecutor(comment)
     }
-    
+
     func warn(_ warning: String, to filePath: String, at lineNumber: Int) {
         inlineWarningExecutor(warning, filePath, lineNumber)
     }
-    
+
     func warn(_ warning: String) {
         normalWarningExecutor(warning)
     }
-    
+
     func fail(_ failure: String, to filePath: String, at lineNumber: Int) {
         inlineFailureExecutor(failure, filePath, lineNumber)
     }
-    
+
     func fail(_ failure: String) {
         normalFailureExecutor(failure)
     }
-    
 }
 
-
-
 extension Kantoku {
-    
     private func postIssuesIfNeeded(from resultFile: XCResultFile, configuration: XCResultParsingConfiguration) {
-        
         if configuration.needsIssues {
-            
             guard let issues = resultFile.getInvocationRecord()?.issues else {
                 warn("Failed to get invocation record from \(resultFile.url.absoluteString)")
                 return
             }
 
             if configuration.parseBuildWarnings {
-                let filteredSummaries = summaries(of: issues.warningSummaries, filteredBy:  configuration.reportingFileType)
+                let filteredSummaries = summaries(of: issues.warningSummaries, filteredBy: configuration.reportingFileType)
                 post(filteredSummaries, as: .warning)
             }
-            
+
             if configuration.parseBuildErrors {
                 post(issues.errorSummaries, as: .failure)
             }
-            
+
             if configuration.parseAnalyzerWarnings {
                 post(issues.analyzerWarningSummaries, as: .warning)
             }
-            
+
             if configuration.parseTestFailures {
                 post(issues.testFailureSummaries, as: .failure)
             }
-            
         }
-        
     }
-    
+
     private func postCoverageIfNeeded(from resultFile: XCResultFile, configuration: XCResultParsingConfiguration) {
-        
         if let coverageAcceptanceDecision = configuration.codeCoverageRequirement.acceptanceDecision {
-            
             guard let coverage = resultFile.getCodeCoverage() else {
-                warn("Failed to get coverage from \(resultFile.url.absoluteString)")
+                if configuration.failIfCoverageUnavailable {
+                    fail("Failed to get coverage from \(resultFile.url.absoluteString)")
+                } else {
+                    warn("Failed to get coverage from \(resultFile.url.absoluteString)")
+                }
+
                 return
             }
-            
-            post(coverage, as: coverageAcceptanceDecision)
-            
+            let excludeCoverageTarget = configuration.excludeCoverageTarget + [.regex("*Tests$")]
+            var relaventTestCoverage = coverage.filterTargets(excludedTargets: excludeCoverageTarget)
+            relaventTestCoverage = coverage.filterTarget(excludeFiles: configuration.excludeCoverageForFiles)
+            let files: [XCResultParsingConfiguration.RelativeFilePath]
+            if configuration.showCoverageForChangedFiles {
+                files = (createdFiles + modifiedFiles)
+
+            } else {
+                files = []
+            }
+            post(relaventTestCoverage, for: files, as: coverageAcceptanceDecision)
         }
-        
     }
-    
+
     public func parseXCResultFile(at filePath: String, configuration: XCResultParsingConfiguration) {
-        
         let resultFile = XCResultFile(url: .init(fileURLWithPath: filePath))
-        
+
         postIssuesIfNeeded(from: resultFile, configuration: configuration)
         postCoverageIfNeeded(from: resultFile, configuration: configuration)
-        
     }
-    
 }
 
 extension XCResultParsingConfiguration.CodeCoverageRequirement {
-    
     var acceptanceDecision: ((Double) -> Kantoku.CoverageAcceptance)? {
         switch self {
         case .none:
             return nil
-            
+
         case .required(let threshold):
             return { coverage in
                 if coverage >= threshold.recommended {
@@ -162,13 +157,10 @@ extension XCResultParsingConfiguration.CodeCoverageRequirement {
             }
         }
     }
-    
 }
 
 extension Kantoku {
-
     private func summaries<T: PostableIssueSummary>(of summaries: [T], filteredBy fileType: XCResultParsingConfiguration.ReportingFileType) -> [T] {
-
         let filteringPredicate: (XCResultParsingConfiguration.RelativeFilePath) -> Bool
 
         switch fileType {
@@ -188,7 +180,5 @@ extension Kantoku {
             }
             return filteringPredicate(relativePath.filePath)
         }
-
     }
-
 }
